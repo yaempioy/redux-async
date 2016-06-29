2ac629b673a7fe5b3955c154bf7014eb6daa7175
kitti16 <kitti.b@ensogo.com>
2016-06-30 00:55:21 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+import webpack from 'webpack'
+import webpackDevMiddleware from 'webpack-dev-middleware'
+import webpackHotMiddleware from 'webpack-hot-middleware'
+import config from './webpack.config'
+import express from 'express'
+
+let app = new express()
+const port = 3000
+
+let compiler = webpack(config)
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+app.use(webpackHotMiddleware(compiler))
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(port, function(error) {
+  if (error) {
+    console.error(error)
+  } else {
+    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+  }
+})
